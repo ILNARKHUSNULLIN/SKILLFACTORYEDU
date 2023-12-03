5eb189e414d26200427bbfa0ad256cdef232eb2c
ILNARKHUSNULLIN <151405830+ILNARKHUSNULLIN@users.noreply.github.com>
2023-12-04 02:05:44 +0300

    Крестики-нолики

---
+++ ox_game pr.py
@@ -0,0 +1,61 @@
+def o_x_game():
+    print('Приветствую тебя в игре Крестики-нолики!')
+    play_area = [[' ', '1', '2', '3'],
+                 ['1', '-', '-', '-'],
+                 ['2', '-', '-', '-'],
+                 ['3', '-', '-', '-']
+                 ]
+
+    win_init = [[[1,1], [1,2], [1,3]], [[1,1], [2,2], [3,3]], [[1,1], [2,1], [3,1]],
+                [[1,2], [2,2], [3,2]], [[1,3], [2,3], [3,3]], [[1,3], [2,2], [3,1]],
+                [[2,1], [2,2], [2,3]], [[3,1], [3,2], [3,3]]]
+
+    for i in play_area:
+        print(*i)
+
+
+    def o_playing():
+        print('Ход ноликов:')
+        print('Впиши поле, которое хочешь занять. Сначала введи строку, потом столбик')
+        stroka = int(input())
+        stolb = int(input())
+        if play_area[stroka][stolb] == '-':
+            play_area[stroka][stolb] = 'O'
+        else:
+            print("Поле уже занято, выбери другое")
+
+
+    def x_playing():
+        print('Ход крестиков:')
+        print('Впиши поле, которое хочешь занять. Сначала введи строку, потом столбик')
+        stroka = int(input())
+        stolb = int(input())
+        if play_area[stroka][stolb] == '-':
+            play_area[stroka][stolb] = 'X'
+        else:
+            print("Поле уже занято, выбери другое")
+
+    def check_winner():
+        for i in win_init:
+            a = i[0]
+            b = i[1]
+            c = i[2]
+            if play_area[a[0]][a[1]] == play_area[b[0]][b[1]] and play_area[a[0]][a[1]] == play_area[c[0]][c[1]] and play_area[a[0]][a[1]] != '-':
+                print("Победитель:", play_area[a[0]][a[1]])
+                return True
+        return False
+
+    control_num = 0
+    while control_num == 0:
+        o_playing()
+        for i in play_area:
+            print(*i)
+        if check_winner() is True:
+            break
+        x_playing()
+        for i in play_area:
+            print(*i)
+        if check_winner() is True:
+            break
+
+o_x_game()
